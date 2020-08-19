b886a9621c3cfe502bad1d54ad7d065af34fbc97
Joel Stockamp <stockampj@gmail.com>
2020-08-19 12:19:38 -0700

    add embla carousel to project

---
+++ src/views/mainWindow/prevAndNextButton.js
@@ -0,0 +1,16 @@
+const setupPrevNextBtns = (prevBtn, nextBtn, embla) => {
+  prevBtn.addEventListener('click', embla.scrollPrev, false);
+  nextBtn.addEventListener('click', embla.scrollNext, false);
+};
+
+const disablePrevNextBtns = (prevBtn, nextBtn, embla) => {
+  return () => {
+    if (embla.canScrollPrev()) prevBtn.removeAttribute('disabled');
+    else prevBtn.setAttribute('disabled', 'disabled');
+
+    if (embla.canScrollNext()) nextBtn.removeAttribute('disabled');
+    else nextBtn.setAttribute('disabled', 'disabled');
+  };
+};
+
+module.exports = {setupPrevNextBtns, disablePrevNextBtns}
